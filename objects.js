3b37406da31f87d2113f7a57cf379e7157142506
singhrahulkanwar@gmail.com Singh <singhrahulkanwar@gmail.com>
2018-04-05 12:49:33 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,1 +1,7 @@
-var playlist={}
+var playlist={sam:"todaysong",test:"testsong"}
+
+function updatePlaylist (obj,artname,songtitle)
+{
+  Object.assign({updateartist:"songupdate"})
+  return playlist
+}
